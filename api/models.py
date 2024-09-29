071d4e66c3963fb98e38c30fa6772bf25fd25168
Tomas <tomasbazan5@gmail.com>
2024-09-29 20:49:11 -0300

    changes

--- api/models.py
+++ api/models.py
@@ -20,8 +20,29 @@ class Tasks(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
     user_responsable = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True, blank=True
+        User, on_delete=models.CASCADE, null=True, blank=True,
     )
 
     def __str__(self):
         return f"{ self.name  } al projecto: { self.project.name }"
+# class ProjectSerializer(serializers.ModelSerializer):  # Changed to ModelSerializer
+#     owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  # Now expects a user ID
+#
+#     class Meta:
+#         model = Project
+#         fields = ["id", "name", "owner"]
+#
+#     def create(self, validated_data):
+#         validated_data['owner'] = self.context['request'].user
+#         return super().create(validated_data)
+#
+#
+# class TasksSerializer(serializers.HyperlinkedModelSerializer):
+#     user_responsable = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), allow_null=True)
+#
+#     class Meta:
+#         model = Tasks
+#         fields = ["url", "id", "name", "description", "done", "user_responsable"]
+#         extra_kwargs = {
+#             'url': {'view_name': 'tasks-detail', 'lookup_field': 'pk'}  # Ensure correct view for URL
+#         }
--- users/roles.py
+++ users/roles.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
-
 class UserRoles(Enum):
     ADMIN = "admin"
     PROJECT_MANAGER = "project_manager"
-    team_member = "team_member"
+    TEAM_MEMBER = "team_member"
