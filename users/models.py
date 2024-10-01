b64108df5364a81235c4e7a2165d47c1114163fa
Tomas <tomasbazan5@gmail.com>
2024-09-30 22:11:20 -0300

    fix: changed the get_natural_key to use the username

--- users/models.py
+++ users/models.py
@@ -1,12 +1,23 @@
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
 from django.utils import choices
 from .roles import UserRoles
 
+class UserManager(BaseUserManager):
+    def get_by_natural_key(self, username):
+        return self.get(username=username)
 
 class User(AbstractBaseUser):
-    name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
     role = models.CharField(
         max_length=50, choices=[(role.value, role.name) for role in UserRoles]
     )
     email = models.EmailField(unique=True, max_length=255)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def get_by_natural_key(self):
+        return self.username
