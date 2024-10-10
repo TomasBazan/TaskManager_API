f860ef392a5ed8605a1fe3d2926b9506122d09b6
Tomas <tomasbazan5@gmail.com>
2024-10-10 20:13:46 -0300

    security: add authentication requirements

--- api/views.py
+++ api/views.py
@@ -1,14 +1,16 @@
-from django.http import JsonResponse
-from django.shortcuts import render
 from .models import Project, Tasks
 from rest_framework import viewsets
 from .serializer import ProjectSerializer, TasksSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
 
+
 class TasksViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = TasksSerializer
     queryset = Tasks.objects.all()
